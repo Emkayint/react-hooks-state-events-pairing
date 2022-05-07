cd3cca794f6381aa46c2ead09dd4168305d71ad1
Martin <mkmartinoes@gmail.com>
2022-05-07 10:02:57 +0300

    Emoji buttons

--- src/components/SongInfo.js
+++ src/components/SongInfo.js
@@ -1,6 +1,15 @@
 
-function songInfo(){
-
+function songInfo({title, views, createdAt, upvotes, downvotes, comments}){
+  return (
+    <>
+      <h2>{title}</h2>
+      <div className="info">
+        <span>{views} views | premierd {createdAt}</span>
+        <span className="btn">&#x1F44D; { upvotes }</span>
+        <span className="btn">&#x1F44E; { downvotes }</span>
+      </div>
+    </>
+  )
 }
 
 export default songInfo;
